8a8550084619a3db8ea83f534e8741a25c5980d0
echo <echofoxey@gmail.com>
2025-02-04 17:05:06 +0100

    Added audiomanager script (pretty empty rn)
    not sure exactly how we wanna set it up? if we wanna have seperate scripts for music and sfx managers? I have a way I would do it but idunno if it's a good way >w<

---
+++ Assets/Scenes/Echo/Scripts/AudioManager.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AudioManager : MonoBehaviour
+{
+    
+    public static AudioManager Instance;
+    
+    
+    private void Awake()
+    {
+        if (Instance != null && Instance != this) // singleton
+        {
+            Destroy(this);
+        }
+        else
+        {
+            Instance = this;
+        }
+        
+        DontDestroyOnLoad(this);
+        
+    }
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
