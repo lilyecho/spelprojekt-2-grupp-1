8665fd87d88aa95fabd80854dd8e01e82eb4854f
Kroonan02 <149394492+Kroonan02@users.noreply.github.com>
2025-02-08 16:09:01 +0100

    gizmos 4 the people

--- Assets/Scenes/Patrik/Jump/PowerUpVisualizers.cs
+++ Assets/Scenes/Patrik/Jump/PowerUpVisualizers.cs
@@ -1,13 +1,13 @@
 using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.Serialization;
 
+[RequireComponent(typeof(Rigidbody))]
 public class PowerUpVisualizers : MonoBehaviour
 {
     [SerializeField] private PlayerMovementData playerMovementData = null;
     [SerializeField] private PlayerQualities visualizePlayerQualities;
+
+    [SerializeField, Min(4)] private int resolutionAreaMarkers;
     
     [Flags]
     private enum PlayerQualities
@@ -24,21 +24,79 @@ public class PowerUpVisualizers : MonoBehaviour
         //Jump & MegaJump
         if (visualizePlayerQualities.CompareTo(PlayerQualities.Jump) != 0)
         {
-            DrawJump(playerMovementData.GetNormalJump.GetJumpHeight);
+            DrawJumpHeight(playerMovementData.GetNormalJump.GetJumpHeight);
         }
         
         if (visualizePlayerQualities.CompareTo(PlayerQualities.MegaJump) != 0)
         {
-            DrawJump(playerMovementData.GetMegaJump.GetJumpHeight);
+            DrawJumpHeight(playerMovementData.GetMegaJump.GetJumpHeight);
         }
         
-        
+        DrawAreaOfLandingFromPoint(transform.position,4);
     }
 
-    private void DrawJump(float jumpHeight)
+    private void DrawJumpHeight(float jumpHeight)
     {
         Gizmos.color = Color.red;
         Vector3 cubePosition = transform.position + new Vector3(0,jumpHeight,0);
         Gizmos.DrawCube(cubePosition, new Vector3(.1f,.1f,.1f));
     }
+
+    private void DrawAreaOfLandingFromPoint(Vector3 startPosition,int resolution)
+    {
+        Vector3 startDirection = new Vector3(0,0,1);
+        float ratio = playerMovementData.GetGravityMagnitudeDown /
+                      playerMovementData.GetMidAirForces.GetAppliedMagnitude;
+        
+        //float angleOfFall = Mathf.Sin(1);
+        
+        float angleBetweenPoints = 360f / resolution;
+        
+        for (int i = 0; i < resolution; i++)
+        {
+            float angleCurrent = angleBetweenPoints * i;
+            
+            Quaternion rotationY = Quaternion.AngleAxis(angleCurrent, Vector3.up);
+            Quaternion rotationX = Quaternion.AngleAxis(angleOfFall, Vector3.right);
+            
+            Quaternion finalRotation = rotationX * rotationY;
+
+            Vector3 finalDirection = finalRotation * startDirection;
+            
+            //Debug.Log(finalDirection.ToString());
+            
+            /*
+            //2D vector for rotating around axis
+            Vector2 directionTwoD = RotateVectorClock(startDirection, angleCurrent);
+            //3D for the angle of fall
+            float 
+            Vector3 aimDirectionThreeD = new Vector3(directionTwoD.x,,directionTwoD.y);
+            Physics.Raycast(startPosition,aimDirectionThreeD)*/
+        }
+    }
+    
+    private Vector2 RotateVectorCounter(Vector2 inputVector, float angle)
+    {
+        if (angle <= 0) throw new ArgumentException("RotateVectorCounter can't and shouldn't handle angle less or equal to 0");
+        
+        float vectorX = inputVector.x * Mathf.Cos(Mathf.Deg2Rad * angle) +
+                        inputVector.y * -Mathf.Sin(Mathf.Deg2Rad * angle);
+        float vectorY = inputVector.x * Mathf.Sin(Mathf.Deg2Rad * angle) +
+                        inputVector.y * Mathf.Cos(Mathf.Deg2Rad * angle);
+
+        return new Vector2(vectorX, vectorY);
+    }
+    private Vector2 RotateVectorClock(Vector2 inputVector, float angle)
+    {
+        if (angle <= 0) throw new ArgumentException("RotateVectorCounter can't and shouldn't handle angle less or equal to 0");
+        
+        float vectorX = inputVector.x * Mathf.Cos(Mathf.Deg2Rad * angle) +
+                        inputVector.y * Mathf.Sin(Mathf.Deg2Rad * angle);
+        float vectorY = inputVector.x * -Mathf.Sin(Mathf.Deg2Rad * angle) +
+                        inputVector.y * Mathf.Cos(Mathf.Deg2Rad * angle);
+
+        return new Vector2(vectorX, vectorY);
+    }
+    
+    
 }
