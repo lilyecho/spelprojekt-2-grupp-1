ce107e36f3a83740cce31e0ba98f5ad6b113a25b
a23micvi <a23micvi@student.his.se>
2025-02-10 11:13:31 +0100

    Slopes toleranser

--- Assets/Scenes/Michael/Falling.cs
+++ Assets/Scenes/Michael/Falling.cs
@@ -2,6 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
+using UnityEngine.InputSystem.HID;
 
 public class Falling : State
 {
@@ -14,8 +15,8 @@ public class Falling : State
     public override void Enter()
     {
         Debug.Log("FALLING");
-        
-
+        playerBehaviour.ChangeJumpState(playerBehaviour.unableToJump);
+        time = 0;
     }
     public override void Exit()
     {
@@ -27,6 +28,9 @@ public class Falling : State
 
     }
 
+    Quaternion targetRotation;
+    float time;
+
     public override void Update()
     {
         if(CheckForGround())
@@ -34,6 +38,12 @@ public class Falling : State
             playerBehaviour.ChangeState(playerBehaviour.idle);
             playerBehaviour.ChangeJumpState(playerBehaviour.ableToJump);
         }
+
+
+        targetRotation = Quaternion.FromToRotation(playerBehaviour.transform.up, Vector3.up) * playerBehaviour.transform.rotation;
+        playerBehaviour.transform.rotation = Quaternion.Slerp(playerBehaviour.transform.rotation, targetRotation, time);
+
+        time += 2 * Time.deltaTime;
     }
 
     public override void FixedUpdate()
@@ -75,11 +85,24 @@ public class Falling : State
     public bool CheckForGround()
     {
         LayerMask layerToIgnore = 1 << 6;
+        RaycastHit hit;
         foreach (Transform t in playerBehaviour.rayCastPoints)
         {
+            /*
             if (Physics.Raycast(t.position, Vector3.down, playerBehaviour.rayCastLength, ~layerToIgnore))
             {
                 return true;
+            }
+            */
+            if (Physics.Raycast(t.position, Vector3.down, out hit, playerBehaviour.rayCastLength, ~layerToIgnore))
+            {
+                float angle = Vector3.Angle(Vector3.up, hit.normal);
+                //Debug.Log(angle);
+                if (angle < 30f)
+                {
+                    return true;
+                }
+
             }
         }
         return false;
