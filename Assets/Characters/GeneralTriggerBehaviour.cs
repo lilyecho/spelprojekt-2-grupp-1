a20f237e5e15ddd05966644fceee8d3c77cc4a64
Kroonan02 <149394492+Kroonan02@users.noreply.github.com>
2025-02-26 20:54:23 +0100

    Så tror jag fixat en snabb lösning som bara reflekterade mer på unityEvents

---
+++ Assets/Characters/GeneralTriggerBehaviour.cs
@@ -0,0 +1,36 @@
+using UnityEngine;
+using UnityEngine.Events;
+
+[RequireComponent(typeof(Collider))]
+public class GeneralTriggerBehaviour : MonoBehaviour
+{
+    [SerializeField] private UnityEvent onEnter;
+    [SerializeField] private UnityEvent onStay;
+    [SerializeField] private UnityEvent onExit;
+
+    private void OnValidate()
+    {
+        Collider validateCollider = GetComponent<Collider>();
+        if (!validateCollider.isTrigger)
+        {
+            Debug.Log(gameObject.name +" activates trigger quality in collider");
+            validateCollider.isTrigger = true;
+        }
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        onEnter.Invoke();
+    }
+
+    private void OnTriggerStay(Collider other)
+    {
+        onStay.Invoke();
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        onExit.Invoke();
+    }
+}
+
