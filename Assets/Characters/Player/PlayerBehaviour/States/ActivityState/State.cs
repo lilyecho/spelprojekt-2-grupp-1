fdeb8ab8bfc39a9a08108b0b37307e5168158993
a23micvi <a23micvi@student.his.se>
2025-03-10 13:48:19 +0100

    Den riktiga sista förändringen i slope-beteende

--- Assets/Characters/Player/PlayerBehaviour/States/ActivityState/State.cs
+++ Assets/Characters/Player/PlayerBehaviour/States/ActivityState/State.cs
@@ -99,7 +99,8 @@ public abstract class State
         else
         {
             //targetRotation = Quaternion.FromToRotation(playerTransform.up, normal) * playerTransform.rotation;
-            targetRotation = Quaternion.FromToRotation(playerTransform.up, Vector3.up)/* * playerTransform.rotation*/;
+            //targetRotation = Quaternion.FromToRotation(playerTransform.up, Vector3.up)/* * playerTransform.rotation*/;
+            targetRotation = Quaternion.Euler(playerTransform.rotation.x, 0, 0); ;
         }
 
         
