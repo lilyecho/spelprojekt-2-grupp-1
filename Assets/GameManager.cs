38055c66e5463619e9ed34caba3417eb17b79561
a23micvi <a23micvi@student.his.se>
2025-03-11 14:59:19 +0100

    Game manager och meny. Inte klara

--- Assets/GameManager.cs
+++ Assets/GameManager.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
@@ -10,7 +11,7 @@ public class GameManager : MonoBehaviour
     public GameObject paused;
     public GameObject settings;
     public GameObject controls;
-
+    public Button defaultButton;
 
     public TimeManager timeManager;
 
@@ -30,7 +31,7 @@ public class GameManager : MonoBehaviour
 
     private void Start()
     {
-        pauseMenu.gameObject.SetActive(true);
+        PauseGame();
     }
 
     
@@ -39,8 +40,9 @@ public class GameManager : MonoBehaviour
 
     public void PauseGame()
     {
-        timeManager.OnMovement(false);
+        timeManager.Movement(false);
         pauseMenu.gameObject.SetActive(true);
+        defaultButton.Select();
     }
     
 
