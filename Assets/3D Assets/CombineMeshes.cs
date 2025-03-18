57fe8994c1764d9759da454b47de55e3097192b3
a23patkr <a23patkr@student.his.se>
2025-03-18 12:15:16 +0100

    Behöver update av bushes

--- Assets/3D Assets/CombineMeshes.cs
+++ Assets/3D Assets/CombineMeshes.cs
@@ -7,20 +7,30 @@ public class CombineMeshes : MonoBehaviour
 {
     void Start()
     {
-        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>();
+        Vector3 pos = transform.position;
+        Quaternion rot = transform.rotation;
+
+        transform.position = Vector3.zero;
+        transform.rotation = Quaternion.identity;
+        
+        MeshFilter[] meshFilters = GetComponentsInChildren<MeshFilter>()[1..];
         CombineInstance[] combine = new CombineInstance[meshFilters.Length];
 
         for (int i = 0; i < meshFilters.Length; i++)
         {
+            Debug.Log(meshFilters[i].mesh.isReadable);
             combine[i].mesh = meshFilters[i].sharedMesh;
             combine[i].transform = meshFilters[i].transform.localToWorldMatrix;
             meshFilters[i].gameObject.SetActive(false);
         }
 
         Mesh mesh = new Mesh();
-        mesh.CombineMeshes(combine);
+        mesh.CombineMeshes(combine, true);
         transform.GetComponent<MeshFilter>().sharedMesh = mesh;
         gameObject.GetComponent<MeshCollider>().sharedMesh = mesh;
         gameObject.SetActive(true);
+        transform.position = pos;
+        transform.rotation = rot;
+
     }
 }
