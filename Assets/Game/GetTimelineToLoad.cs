5befc47449bafe1f0490cbfeef480c3702fe5e8c
a23patkr <a23patkr@student.his.se>
2025-03-27 14:41:18 +0100

    fix cinematic

--- Assets/Game/GetTimelineToLoad.cs
+++ Assets/Game/GetTimelineToLoad.cs
@@ -10,21 +10,16 @@ public class GetTimelineToLoad : MonoBehaviour
 
     [SerializeField] private string sceneToLoad;
 
-    //private bool isSceneChanging = false;
-
     private void FixedUpdate()
     {
-        Debug.Log(videoPlayer.isPaused);
         if (videoPlayer.isPaused)
         {
-            Debug.Log("ok");
             ChangeScene();
         }
     }
 
     private void ChangeScene()
     {
-
         SceneManager.LoadScene(sceneToLoad);
     }
 }
