b668fd9de50f34dc1731f74590e74fca8bd320f4
a23micvi <a23micvi@student.his.se>
2025-03-27 09:09:49 +0100

    Döljer musen under gameplay

--- Assets/MainMenu.cs
+++ Assets/MainMenu.cs
@@ -23,4 +23,14 @@ public class MainMenu : MonoBehaviour
     {
         Application.Quit();
     }
+
+    private void Start()
+    {
+        Cursor.visible = true;
+    }
+
+    public void HideCursor()
+    {
+        Cursor.visible = false;
+    }
 }
